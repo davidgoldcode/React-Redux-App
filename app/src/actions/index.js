02c675665e0aa8fbf16994ab55dad8d425a0c48d
David Gold <davidgold@hey.com>
2020-09-09 16:48:30 -0400

    first take on the actions

--- app/src/actions/index.js
+++ app/src/actions/index.js
@@ -1,8 +1,24 @@
 import axios from 'axios';
 
+// api link
+const apiUrl = 'http://taco-randomizer.herokuapp.com/random/';
+
 // action types
 export const FETCH_RECIPE = 'FETCH_RECIPE';
 export const FETCH_RECIPE_SUCCESS = 'FETCH_RECIPE_SUCCESS';
 export const FETCH_RECIPE_ERROR = 'FETCH_RECIPE_ERROR';
 
 // action creators
+export const fetchRecipe = () => (dispatch) => {
+    dispatch({ type: FETCH_RECIPE });
+    axios 
+        .get(apiUrl)
+        .then(res => {
+            debugger;
+            dispatch({ type: FETCH_RECIPE_SUCCESS, payload: res.data.all }); // too fix
+        })
+        .catch(err => {
+            debugger;
+            dispatch({ type: FETCH_RECIPE_ERROR, payload: { message: "No tacos for you, friend"} });
+        })
+    }
--- app/src/reducers/index.js
+++ app/src/reducers/index.js
@@ -1,13 +1,43 @@
 
 const initialState = {
-    recipes: ['recipe 1', 'recipe 2', 'recipe 3'], 
+    recipes: ['recipe 123', 'recipe 7'], 
     loadingTacos: true,
     errorMessage: '',
 }
 
+export const FETCH_RECIPE = 'FETCH_RECIPE';
+export const FETCH_RECIPE_SUCCESS = 'FETCH_RECIPE_SUCCESS';
+export const FETCH_RECIPE_ERROR = 'FETCH_RECIPE_ERROR';
+
+
 export default (state = initialState, action) => {
     switch(action.type) {
+        case FETCH_RECIPE:
+            return {
+                ...state,
+                loadingTacos: true,
+            };
+        case FETCH_RECIPE_ERROR:
+            return {
+                ...state,
+                loadingTacos: false,
+                errorMessage: action.payload.message,
+            };
+        case FETCH_RECIPE_SUCCESS:
+            return {
+            ...state,
+            recipes: [...state.recipes, action.payload],
+            loadingTacos: false,
+        }
         default: 
             return state;
     }
 }
+
+// {
+//     base_layer: '',
+//     condiment: '',
+//     mixin: '',
+//     seasoning: '',
+//     shell: '',
+// }
