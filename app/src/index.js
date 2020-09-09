f6ca9580ab09f1e8b3b2ff7997f3d9bdeb9e6068
David Gold <davidgold@hey.com>
2020-09-09 16:15:43 -0400

    added axios in

--- app/src/index.js
+++ app/src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {applyMiddleware, createStore} from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import reducer from './reducers/index'
+
+const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ app/src/reducers/index.js
@@ -0,0 +1,13 @@
+
+const initialState = {
+    recipes: [], 
+    loadingTacos: true,
+    errorMessage: '',
+}
+
+export default (state = initialState, action) => {
+    switch(action.type) {
+        default: 
+            return state;
+    }
+}
